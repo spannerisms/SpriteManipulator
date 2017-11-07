c156ef075d79eb5950b823c005881734318586f4
fatmanspanda <32842036+fatmanspanda@users.noreply.github.com>
2017-11-07 14:59:35 -0500

    JFileChooser specifically for editing sprites

---
+++ BetterJFileChooser.java
@@ -0,0 +1,58 @@
+package SpriteManipulator;
+
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileFilter;
+
+/**
+ * Need alternative confirm messages
+ */
+public class BetterJFileChooser extends JFileChooser {
+	private static final long serialVersionUID = -7581065406880416887L;
+	private boolean patchingROM;
+	public void approveSelection() {
+		File f = getSelectedFile();
+		if(f.exists() && getDialogType() == SAVE_DIALOG){
+			String warning;
+			String warningHeader;
+			if (patchingROM) {
+				warning =
+						"Are you sure you want to patch \"" + f.getName() + "\"?\n"+
+						"This cannot be undone.";
+				warningHeader = "This is a ROM";
+			} else {
+				warning = "The file \"" + f.getName() + "\" already exists. Save anyway?";
+				warningHeader = "Existing file";
+			}
+			int result = JOptionPane.showConfirmDialog(
+					this,
+					warning,
+					warningHeader,
+					JOptionPane.YES_NO_CANCEL_OPTION);
+			switch(result){
+				case JOptionPane.YES_OPTION :
+					super.approveSelection();
+					return;
+				case JOptionPane.NO_OPTION :
+				case JOptionPane.CLOSED_OPTION :
+					return;
+				case JOptionPane.CANCEL_OPTION:
+					cancelSelection();
+					return;
+			}
+		}
+		super.approveSelection();
+	}
+		public void setROMStatus(boolean b) {
+		patchingROM = b;
+	}
+
+	public void removeAllFilters() {
+		FileFilter[] exlist = this.getChoosableFileFilters();
+		for (FileFilter r : exlist) {
+			this.removeChoosableFileFilter(r);
+		}
+	}
+}
